7d9271e83a68e36b4845ed447ccb37cc9da58e03
João Sereia <joao.lo.sereia@gmail.com>
2025-11-27 18:43:45 -0300

    chore(android): configure support for wear os

--- android/app/src/main/kotlin/br/com/softyes/tabnews/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/softyes/tabnews/MainActivity.kt
@@ -1,5 +1,13 @@
 package br.com.softyes.tabnews
 
+import android.os.Bundle
+import android.view.MotionEvent
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    intent.putExtra("background_mode", "transparent")
+    super.onCreate(savedInstanceState)
+  }
+}
